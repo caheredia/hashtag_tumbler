a8b3d6865cf44a90c45bbc62c2f8cb89a1e45180
caheredia <cristianheredia@gmail.com>
2019-09-11 00:28:44 -0700

    create fetch script

---
+++ fetch_results.py
@@ -0,0 +1,14 @@
+import sqlite3
+import datetime
+import time
+
+conn = sqlite3.connect("hashtag.db")
+
+c = conn.cursor()
+
+c.execute(
+    """SELECT method, AVG(rate) FROM rates
+GROUP BY method
+"""
+)
+print(c.fetchall())
--- writes_sql.py
+++ writes_sql.py
@@ -6,6 +6,10 @@ conn = sqlite3.connect("hashtag.db")
 
 c = conn.cursor()
 
+# print initial row count
+c.execute("SELECT COUNT(*) FROM hashtags")
+print(c.fetchall())
+
 
 def add_tag(tag):
     c.execute(
@@ -15,20 +19,39 @@ def add_tag(tag):
     conn.commit()
 
 
-time_now = datetime.datetime.now().isoformat()
+def save_rate(write_rate):
+    """Save rates to rate table."""
+    c.execute(
+        "INSERT INTO rates VALUES (:method,:rate)",
+        {"method": "sql", "rate": write_rate},
+    )
+    conn.commit()
 
 
-start = time.time()
-row = 1000
-for i in range(row):
-    add_tag(time_now)
-# conn.commit()
-end = time.time()
-delta = end - start
-print(f"toatl time: {delta}")
-write_rate = int(row / delta)
-print(f"Rows/second: {write_rate}")
+def calculate_write_rate(rows):
+    """Returns write rate to write integer number of rows."""
+    start = time.time()
+    for i in range(rows):
+        time_now = datetime.datetime.now().isoformat()
+        add_tag(time_now)
+    end = time.time()
+    delta = end - start
+    print(f"toatl time: {delta}")
+    write_rate = int(rows / delta)
+    print(f"Rows/second: {write_rate}")
+    return write_rate
 
+
+def multiple_runs(rows, runs):
+    """"Run writes multiple times and save results to rates table."""
+    for i in range(runs):
+        write_rate = calculate_write_rate(rows)
+        save_rate(write_rate)
+
+
+multiple_runs(rows=1000, runs=10)
+
+# print final row count
 c.execute("SELECT COUNT(*) FROM hashtags")
 print(c.fetchall())
 
