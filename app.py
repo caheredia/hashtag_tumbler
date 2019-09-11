c40c209ee79ebb9fbb819e2c313be12e2514ef70
caheredia <cristianheredia@gmail.com>
2019-09-10 23:08:18 -0700

    make all calls async

--- app.py
+++ app.py
@@ -21,35 +21,18 @@ async def before_start(app, loop):
 @app.listener("after_server_stop")
 async def after_stop(app, loop):
     print("Closing connection to sqlite")
-    # conn.close()
     await db.close()
 
 
 @app.route("/total", methods=["GET"])
 async def post(request):
-    conn = sqlite3.connect("hashtag.db")
-    c = conn.cursor()
-    c.execute("SELECT COUNT(*) FROM hashtags")
-    data = c.fetchall()
-    conn.close()
-    return text(data)
+    cursor = await db.execute("SELECT COUNT(*) FROM hashtags")
+    rows = await cursor.fetchall()
+    await cursor.close()
+    return text(rows)
 
 
 @app.route("/tag", methods=["POST"])
-async def post(request):
-    tag = request.json["tag"]
-    conn = sqlite3.connect("hashtag.db")
-    c = conn.cursor()
-    c.execute(
-        "INSERT INTO hashtags VALUES (:user,:category,:tag)",
-        {"user": "xristian", "category": "leica", "tag": tag},
-    )
-    conn.commit()
-    conn.close()
-    return text("success")
-
-
-@app.route("/async-tag", methods=["POST"])
 async def post(request):
     tag = request.json["tag"]
     await db.execute(
@@ -60,12 +43,5 @@ async def post(request):
     return text("success")
 
 
-asyncio.set_event_loop(uvloop.new_event_loop())
-server = app.create_server(host="0.0.0.0", port=8000, return_asyncio_server=True)
-loop = asyncio.get_event_loop()
-task = asyncio.ensure_future(server)
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    loop.stop()
-    sys.exit(0)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
