6e7d0614515e4cc9104051fccab8325b6c3622b6
Cristian <cheredia@kountable.com>
2019-09-14 00:15:14 -0700

    fix aiohttp

--- writes_async.py
+++ writes_async.py
@@ -7,47 +7,42 @@ import uvloop
 import sqlite3
 
 
-r = requests.get("http://localhost:8000/total")
-print("number of rows: ", r.text)
-
 
 time_now = datetime.datetime.now().isoformat()
+url = "http://localhost:8000/tag"
+save_url = "http://localhost:8000/save"
 
 
-async def add_tag():
-    payload = {"tag": time_now}
-    await async_requests.post("http://localhost:8000/tag", json=payload)
+async def curl(session, url, method="GET", json=None):
+    async with session.request(method, url, json=json) as response:
+        return await response.text()
 
 
-async def bulk_calls(tasks):
-    await asyncio.gather(*tasks)
+async def main():
+    async with aiohttp.ClientSession() as session:
+        runs = 10
+        rows = 100
+        for i in range(runs):
+            tasks = []
+            start = time.time()
+            for i in range(rows):
+                payload = {"tag": time_now}
+                tasks.append(curl(session, url, "POST", json=payload))
+            await asyncio.gather(*tasks)
+            end = time.time()
+            delta = end - start
+            print(f"total time: {delta}")
+            write_rate = int(rows / delta)
+            print(f"Rows/second: {write_rate}")
+            # save write speeds
+            write_payload = {"method": "aiohttp_sanic_uvloop", "rate": write_rate}
+            await curl(session, save_url, method="POST", json=write_payload)
 
 
-def save_rate(method, write_rate):
-    """Save rates to rate table."""
-    conn = sqlite3.connect("hashtag.db")
-    c = conn.cursor()
-    c.execute(
-        "INSERT INTO rates VALUES (:method,:rate)",
-        {"method": method, "rate": write_rate},
-    )
-    conn.commit()
-    conn.close()
-
-
-runs = 10
-rows = 100
-for i in range(runs):
-    tasks = []
-    start = time.time()
-    for i in range(rows):
-        tasks.append(add_tag())
+if __name__ == "__main__":
+    r = requests.get("http://localhost:8000/total/hashtags")
+    print("number of rows: ", r.json()["total"])
     uvloop.install()
-    asyncio.run(bulk_calls(tasks))
-    end = time.time()
-    delta = end - start
-    print(f"total time: {delta}")
-
-    write_rate = int(rows / delta)
-    print(f"Rows/second: {write_rate}")
-    save_rate(method="async_uvloop", write_rate=write_rate)
+    asyncio.run(main())
+    r = requests.get("http://localhost:8000/total/hashtags")
+    print("number of rows: ", r.json()["total"])
