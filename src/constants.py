a0eaf999149ac3d16095a11bd75adb06541fa491
Cristian Heredia <cheredia@wattlearn.com>
2020-11-23 20:44:30 -0800

    Removed all files in .gitignore

--- src/constants.py
+++
@@ -1,1 +0,0 @@
-DATABASE_FILE = "src/database/hashtags.db"
--- src/test_app.py
+++
@@ -1,7 +0,0 @@
-from src.app import app
-
-
-def test_health():
-    _, response = app.test_client.get('/health')
-    assert response.status == 200
-    assert response.json['database open'] is True
