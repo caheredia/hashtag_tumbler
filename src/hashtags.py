241517726a8c51965343ea384de0490dc709ef0f
Cristian Heredia <cheredia@wattlearn.com>
2020-11-27 15:18:46 -0800

    added main file

--- src/hashtags.py
+++ src/hashtags.py
@@ -24,7 +24,3 @@ def return_hashtags(hashtag_set: set) -> str:
     return "".join(hashtag_string)
 
 
-# sudo main routine
-parsed_hashtags = parse_hashtag_string(foo)
-random_parsed_set = k_random_hashtags(parsed_hashtags, 28)
-print(return_hashtags(random_parsed_set))
--- src/main.py
+++ src/main.py
@@ -4,3 +4,7 @@ from src.hashtags import (foo, k_random_hashtags, parse_hashtag_string,
 parsed_hashtags = parse_hashtag_string(foo)
 random_parsed_set = k_random_hashtags(parsed_hashtags, 28)
 print(return_hashtags(random_parsed_set))
+
+# TODO make this a main file, capable of receiving individual groups and a number 
+# for example, --fuji 10 --leica 4 --landscape 4 --sanfrancsico
+# and fill the remaing set with with total , defaulted to 30
