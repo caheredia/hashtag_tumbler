34c1c389536a959700fb298eff88f6acb1c7f563
Cristian Heredia <cheredia@wattlearn.com>
2020-11-27 15:09:05 -0800

    added function to return k random tags

--- src/hashtags.py
+++ src/hashtags.py
@@ -1,4 +1,6 @@
-foo = """#gw690 #fujigw690 #kodakfilm #filmphotography #shootfilm  #filmshooters #fineartfilm #exploremore #lensculture #lensculturestreets #ig_street #streetshot  #analog #6x9 #texasleica #madewithkodak #shootfilmmag  #analoguevibes  #filmphotographic #bayarea #filmwave  #dreamermagazine  #onearthmagazine #architecture #sanfrancisco #shootfilm #chicano #streetphotography #ektachrome100 #latinxartist"""
+from random import randint
+
+foo = """#gw690 #fujigw690 #kodakfilm #filmphotography #shootfilm  #filmshooter #fineartfilm #exploremore #lensculture #lensculturestreets #ig_street #streetshot  #analog #6x9 #texasleica #madewithkodak #shootfilmmag  #analoguevibes  #filmphotographic #bayarea #filmwave  #dreamermagazine  #onearthmagazine #architecture #sanfrancisco #shootfilm #chicano #streetphotography #ektachrome100 #latinxartist"""
 
 
 def parse_hashtag_string(hashtags: str) -> set:
@@ -6,10 +8,23 @@ def parse_hashtag_string(hashtags: str) -> set:
     return set([item.strip() for item in hashtags.split("#") if item != ""])
 
 
+def k_random_hashtags(hashtag_set: set, k: int) -> set:
+    """Returns a k size set of elements."""
+    hashtag_list = list(hashtag_set)
+    random_elements = [
+        hashtag_list.pop(randint(0, len(hashtag_list) - 1)) for _ in range(k)
+    ]
+    return set(random_elements)
+
+
 def return_hashtags(hashtag_set: set) -> str:
     """Returns a string of formatted hashtags."""
-    hashtag_string = ""
-    for tag in hashtag_set:
-        hashtag_string += f"#{tag} "
+    hashtag_string = [f"#{tag} " for tag in hashtag_set]
 
-    return hashtag_string
+    return "".join(hashtag_string)
+
+
+# sudo main routine
+parsed_hashtags = parse_hashtag_string(foo)
+random_parsed_set = k_random_hashtags(parsed_hashtags, 28)
+print(return_hashtags(random_parsed_set))
